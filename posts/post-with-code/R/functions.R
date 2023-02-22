26cdd70b1cabb9ffe7919bf1104ab305da48d499
gabammour <gabriel.ammour1@etu.univ-nantes.fr>
2023-02-22 16:53:39 +0100

    final

--- posts/post-with-code/R/functions.R
+++ posts/post-with-code/R/functions.R
@@ -1,7 +1,3 @@
-###############################
-# Fonction : centre_segment ----
-################################
-
 #' centre_segment 
 #' @description Cette fonction calcule le centre d'un segment 
 #' @param x_a Coordonnées x d'un premier point 
@@ -15,9 +11,6 @@ centre_segment <- function(x_a,y_a,x_b,y_b){
   return(c((x_a+x_b)/2,(y_a+y_b)/2))
 }
 
-################################
-# Fonction : divide_triangle ----
-################################
 
 #' divide_triangle 
 #' @description Cette fonction utilise l'algorithme de  Sierpiński pour 
@@ -47,31 +40,25 @@ divide_triangle <-
   }
 
 
-################################
-# Fonction : divide_liste_triangle  ----
-################################
 
-#' divide_liste_triangle 
+#' divide_list_triangle 
 #' @description Cette fonciton prend une liste de triangle et renvoie une liste 
 #' composé des 4 triangles issus de la décomposition de Sierpinski. 
 #'
-#' @param liste_triangle Une liste de coordonées de triangles 
+#' @param list_triangle Une liste de coordonées de triangles 
 #'
 #' @return liste 4 fois plus grandes, on applique l'algorithme de Sierpinski aux 
 #' triangles donnés en entrée. 
 #' @export
 #'
-divide_list_triangle <- function(liste_triangle){ 
+divide_list_triangle <- function(list_triangle){ 
   sous_liste <- list()
-  for (i in liste_triangle){ 
+  for (i in list_triangle){ 
     sous_liste <- append(sous_liste, divide_triangle(i))
   }
   return(sous_liste)
 }
 
-################################
-# Fonction : distance_cart ----
-################################
 
 #' distance_cart : distance cartèsienne 
 #' @description Cette fonction calcul la distance cartésienne entre deux points 
@@ -87,41 +74,38 @@ distance_cart <- function(x1, y1, x2, y2) {
 }
 
 
-################################
-# Fonction : heron_liste ----
-################################
 
-#' heron_liste 
+#' heron_list 
 #' 
 #' @description Cette fonction prend en entrée une liste de triangle et 
 #' renvoie la somme des aires de ces triangles. 
 #'
-#' @param liste_triangle C'est une liste de triangle du type : 
-#' liste_triangle = list([x11,y11,x21,y21,x31,y31],[x12,y12,x22,y22,x32,y32] ... )
+#' @param list_triangle C'est une liste de triangle du type : 
+#' list_triangle = list([x11,y11,x21,y21,x31,y31],[x12,y12,x22,y22,x32,y32] ... )
 #'
 #' @return Elle renvoie l'air de ce triangle 
 #' @import heron
 #' @export
 #' 
-heron_liste <- function(liste_triangle) {
+heron_list <- function(list_triangle) {
   sum_aire <- 0
   # On extrait les distances entre les sommets 
-  for (i in seq_along(liste_triangle)) {
+  for (i in seq_along(list_triangle)) {
     A_B <-
-      distance_cart(liste_triangle[[i]][1],
-                    liste_triangle[[i]][2],
-                    liste_triangle[[i]][3],
-                    liste_triangle[[i]][4])
+      distance_cart(list_triangle[[i]][1],
+                    list_triangle[[i]][2],
+                    list_triangle[[i]][3],
+                    list_triangle[[i]][4])
     B_C <-
-      distance_cart(liste_triangle[[i]][3],
-                    liste_triangle[[i]][4],
-                    liste_triangle[[i]][5],
-                    liste_triangle[[i]][6])
+      distance_cart(list_triangle[[i]][3],
+                    list_triangle[[i]][4],
+                    list_triangle[[i]][5],
+                    list_triangle[[i]][6])
     C_A <-
-      distance_cart(liste_triangle[[i]][5],
-                    liste_triangle[[i]][6],
-                    liste_triangle[[i]][1],
-                    liste_triangle[[i]][2])
+      distance_cart(list_triangle[[i]][5],
+                    list_triangle[[i]][6],
+                    list_triangle[[i]][1],
+                    list_triangle[[i]][2])
     
     # On calcule l'air des triangles 
     sum_aire <- sum_aire + heron(A_B, B_C, C_A)
@@ -130,27 +114,24 @@ heron_liste <- function(liste_triangle) {
 }
 
 
-################################
-# Fonction : plot_triangles
-################################
 
 #' plot_triangles 
 #' @description #' Ainsi cette fonction permet de tracer tous les triangles dont les coordonnées sont rentrées dans 
 #' une liste. 
 #' @import ggplot2 
-#' @param liste_triangle le premier paramètres est une liste de triangles ordonnés de cette manière : 
-#' liste_triangle = list([x11,y11,x21,y21,x31,y31],[x12,y12,x22,y22,x32,y32] ... ). 
+#' @param list_triangle le premier paramètres est une liste de triangles ordonnés de cette manière : 
+#' list_triangle = list([x11,y11,x21,y21,x31,y31],[x12,y12,x22,y22,x32,y32] ... ). 
 #'
 #' @return Cette fonction affiche sur un graphique tous les triangles. 
 #' @export 
-plot_triangles <- function(liste_triangle) {
+plot_triangles <- function(list_triangle) {
   df <- data.frame()
   
   #Construction d'un data frame avec les coordonnées des triangles
-  for (i in seq_along(liste_triangle)) {
+  for (i in seq_along(list_triangle)) {
     df <- rbind(df, data.frame(
-      x = c(liste_triangle[[i]][1], liste_triangle[[i]][3], liste_triangle[[i]][5]),
-      y = c(liste_triangle[[i]][2], liste_triangle[[i]][4], liste_triangle[[i]][6]),
+      x = c(list_triangle[[i]][1], list_triangle[[i]][3], list_triangle[[i]][5]),
+      y = c(list_triangle[[i]][2], list_triangle[[i]][4], list_triangle[[i]][6]),
       triangle_id = i
     ))
   }
@@ -158,7 +139,7 @@ plot_triangles <- function(liste_triangle) {
   #On trace les triangles
   plot <- ggplot(df, aes(x, y, fill = factor(triangle_id))) +
     geom_polygon(color = "black") +
-    scale_fill_manual(values = rep("#191970", length(liste_triangle)), guide = "none") +
+    scale_fill_manual(values = rep("#191970", length(list_triangle)), guide = "none") +
     theme(axis.text.x=element_blank(), #remove x axis labels
           axis.ticks.x=element_blank(), #remove x axis ticks
           axis.text.y=element_blank(),  #remove y axis labels
--- posts/post-with-code/_targets.R
+++ posts/post-with-code/_targets.R
@@ -16,16 +16,16 @@ triangle_initial <- c(0,1,0,0,0.5,sqrt(3)/2) #c(x1,y1,x2,y2,x3,y3)
 list(
   tar_target("Iteration_1", divide_triangle(triangle_initial)),
   tar_target("plot_1", plot_triangles(Iteration_1)),
-  tar_target("Aire_1", heron_liste(Iteration_1)),
+  tar_target("Aire_1", heron_list(Iteration_1)),
   tar_target("Iteration_2", divide_list_triangle(Iteration_1)),
   tar_target("plot_2", plot_triangles(Iteration_2)),
-  tar_target("Aire_2", heron_liste(Iteration_2)),
+  tar_target("Aire_2", heron_list(Iteration_2)),
   tar_target("Iteration_3", divide_list_triangle(Iteration_2)),
   tar_target("plot_3", plot_triangles(Iteration_3)),
-  tar_target("Aire_3", heron_liste(Iteration_3)),
+  tar_target("Aire_3", heron_list(Iteration_3)),
   tar_target("Iteration_4", divide_list_triangle(Iteration_3)),
   tar_target("plot_4", plot_triangles(Iteration_4)),
-  tar_target("Aire_4", heron_liste(Iteration_4)),
+  tar_target("Aire_4", heron_list(Iteration_4)),
   tar_target("Render_post", tar_quarto(post.qmd))
 )
 
